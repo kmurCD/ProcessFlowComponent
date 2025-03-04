adbefa5a257d406a067efc36128c36aea376f41b
Josue <josue.centella.espiritu@gmail.com>
2025-03-04 15:55:08 -0500

    new branch development

--- flowProcess/index.ts
+++ flowProcess/index.ts
@@ -8,6 +8,7 @@ export class flowProcess
   private notifyOutputChanged: () => void;
   private phaseValue = 0;
   private phaseValueSelect: number;
+    newPhaseValue: number;
 
   constructor() {
     // Constructor inicializado
@@ -43,14 +44,21 @@ export class flowProcess
         console.log("Phase Select: " +this.phaseValueSelect)
 
         this.notifyOutputChanged();
-      },
+      },  onNewPhaseValue: () => {
+        this.newPhaseValue = this.phaseValue + 1;  // Incrementa el valor en +1
+        console.log("New phase value: " + this.newPhaseValue);
+  
+        this.notifyOutputChanged();  // Notifica el cambio a Dynamics 365
+      }
+
     });
   }
 
  
   public getOutputs(): IOutputs {
     return {
-      ControlPhase: this.phaseValueSelect
+      ControlPhase: this.phaseValueSelect,
+      Phase: this.newPhaseValue > 0 ? this.newPhaseValue : this.phaseValue, 
     };
   }
 
